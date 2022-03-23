1a4dd6873920c2c8c967b887899277387dbab00a
Adrienne Meltzer <adriennemeltzer@gmail.com>
2022-03-23 15:34:17 -0400

    EmployerController done

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Please enter a location. This is a required field")
+    @Size(min=3, max=100, message = "Please enter name between 3 and 100 characters.")
+    private String location;
 
+    public Employer(){}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
